c1e90ce7f2813f160b89b9184938bbef7674b617
ahmed-bulbul <bulbulahmed.cse@yahoo.com>
2025-08-01 23:09:57 +0600

    update s3 bucket

--- src/main/java/com/apps/product/controller/ProductController.java
+++ src/main/java/com/apps/product/controller/ProductController.java
@@ -3,18 +3,23 @@ package com.apps.product.controller;
 
 import com.apps.product.model.Product;
 import com.apps.product.repository.ProductRepository;
+import com.apps.product.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/products")
 public class ProductController {
-    private final ProductRepository repo;
 
-    public ProductController(ProductRepository repo) {
-        this.repo = repo;
-    }
+    private final ProductRepository repo;
+    private final S3Service s3Service;
+
 
     @GetMapping
     public List<Product> getAll() {
@@ -24,5 +29,15 @@ public class ProductController {
     @PostMapping
     public Product create(@RequestBody Product p) {
         return repo.save(p);
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String result = s3Service.uploadFile(file);
+            return ResponseEntity.ok(result);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Error: " + e.getMessage());
+        }
     }
 }
